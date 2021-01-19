46d047ccd708b62a456af1e033091440248e2f1b
Bérengère Carpentier <72983609+carpentierberengere@users.noreply.github.com>
2021-01-19 22:17:32 +0100

    ajout logout dans header dashboard

--- template/index.php
+++ template/index.php
@@ -9,6 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="assets/style.css">
   <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+  <link href="https://use.fontawesome.com/releases/v5.12.0/css/all.css" rel="stylesheet">
 
   <title>Breaking Badge</title>
 </head>
--- template/pages/header.php
+++ template/pages/header.php
@@ -1,6 +1,6 @@
 <div class="header">
   <a href="#default" class="logo"><img src="./assets/logoBB.png" height=50px width=110px/></a>
   <div class="header-right">
-    <a href="#logout">Log Out</a>
+  <a href="./logout.php"><i class="fas fa-sign-out-alt fa-lg"></i></a>
   </div>
 </div> 
