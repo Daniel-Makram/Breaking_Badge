85ac4440ab86e80c43458b1f1527f278bbb7a83e
Bérengère Carpentier <72983609+carpentierberengere@users.noreply.github.com>
2021-01-19 11:00:54 +0100

    formulaire login code html

---
+++ template/pages/login.php
@@ -0,0 +1,18 @@
+<?php
+include_once ('./components/functions.php');
+?>
+
+<form class="formulaire_connexion" method="POST">
+        <p>
+            <label for="email"><b>E-mail </b></label>
+            <input type="email" name="email" placeholder="Enter a valid e-mail adress..."  />
+        </p>
+        <p>
+            <label for="pwd"><b>Password </b></label>
+            <input type="password" name="pwd" placeholder="Enter your password..."  />
+        </p>
+        <p>
+            <input type="submit" id="confirm_connexion" value="Confirm" /> 
+        </p>
+</form>
+
